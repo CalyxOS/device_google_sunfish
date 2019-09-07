f227099e566d6222c21891b674eb0c6479da4a38
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 19:56:16 -0700

    sunfish: Define paths to DTC and MKDTIMG
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -229,6 +229,9 @@ BOARD_SUPER_PARTITION_ERROR_LIMIT := 9231663104
 # Inline kernel building
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/tools-lineage/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_LD := LD=ld.lld
