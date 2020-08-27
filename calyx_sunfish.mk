79baa5c6802961d5bf60046f20e1994e432129fa
Chirayu Desai <chirayudesai1@gmail.com>
2020-08-27 22:28:11 +0530

    Add initial Calyx product makefiles
    Also includes:
    commit bc88fcd262e70cac89223b460611b2fc444d4b13
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Nov 12 03:25:25 2020 +0530
    
        calyx: Inherit device-calyx.mk
    
        Change-Id: If3f761b8178b1663b73eb705d77105bd30ec15a9
        (cherry picked from commit 8d48d9ea51645e468a015465b919c365ecb7da90)
    
    commit a3196e48fe1795cc6acbe37ff847eec6ebc13a9c
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Thu Dec 3 03:43:51 2020 +0530
    
        Add overlay-calyx to the list of overlays
    
        Change-Id: I3221d4d5e29f5beb6554d0c280dec546d29f2c08
        (cherry picked from commit 7266f839ce08df3aa61cc80f28afff3c0d3f77ac)
    
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6
    (cherry picked from commit e3479212d8d5ca8ab070d9ee2069aa706ad1b781)

---
+++ calyx_sunfish.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/sunfish/device-calyx.mk)
+$(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_sunfish
+PRODUCT_MODEL := Pixel 4a
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=sunfish \
+    PRIVATE_BUILD_DESC="sunfish-user 13 TP1A.220624.014 8819323 release-keys" \
+
+BUILD_FINGERPRINT := google/sunfish/sunfish:13/TP1A.220624.014/8819323:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
