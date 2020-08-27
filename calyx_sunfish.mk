03510a85f17255ca47adf488128ba8e759187247
Chirayu Desai <chirayudesai1@gmail.com>
2020-08-27 22:28:11 +0530

    Add initial Calyx product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ calyx_sunfish.mk
@@ -0,0 +1,10 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_sunfish
+PRODUCT_MODEL := Pixel 4a
+PRODUCT_RESTRICT_VENDOR_FILES := false
