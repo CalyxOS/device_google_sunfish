CalyxOS/device_google_coral

Add privapp permissions for Google EUICC apps
Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

device.mk
@@ -955,3 +955,6 @@ PRODUCT_PACKAGES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/coral/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml

CalyxOS/device_google_sunfish

device.mk
@@ -925,3 +925,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota_retrofit.mk)
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/sunfish/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml