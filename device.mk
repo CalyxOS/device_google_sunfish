04708432a224e00fd7615079fbf4be1fb44ee6dd
Peter Cai <peter@typeblog.net>
2020-12-23 17:09:47 +0800

    sunfish: Set up proprietary files extraction & build vendor image
    * Blobs that can be built from source code are built from source, except
      librmnetctl.
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    * Various lib and folder symlinks need to be created.
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Christopher N. Hesse <raymanfx@gmail.com>
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Eamon Powell <eamonpowell@outlook.com>
    Co-authored-by: Josh Chasky <jchasky@gmail.com>
    Co-authored-by: "Josh Fox (XlxFoXxlX)" <joshfox87@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I17f79b417a8f0d022bec9c6b5f37d03b842d43e1
    (cherry picked from commit 16bc73387e352b3cf5e0f20561e39375db29a0c1)

--- device.mk
+++ device.mk
@@ -100,17 +100,12 @@ $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-ifeq ($(wildcard vendor/google_devices/sunfish/proprietary/device-vendor-sunfish.mk),)
-    BUILD_WITHOUT_VENDOR := true
-endif
-
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/google/sunfish-kernel/Image.lz4
 else
     LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/sunfish-kernel/sm7150/kernel-headers
-
 
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 29
