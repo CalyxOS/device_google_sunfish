08f5b195abc400cae915a03840889698c0b2064b
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: I62c04c5d2135f78dad53f1152dbf1cfd473d64dc

--- calyx_sunfish.mk
+++ calyx_sunfish.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/calyx/config/common.mk)
 $(call inherit-product, device/google/sunfish/device-calyx.mk)
 $(call inherit-product, device/google/sunfish/aosp_sunfish.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/sunfish/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_sunfish
 PRODUCT_MODEL := Pixel 4a
