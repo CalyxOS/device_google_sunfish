ad99abe95b0df711c39f6488dec9b5330c829167
Michael Bestas <mkbestas@gmail.com>
2021-09-22 23:44:44 +0300

    calyx: Override product name/fingerprint/description
    Change-Id: Ie673890e0380a1b88ecb010a085e4d3c3fff9c61
    (cherry picked from commit 2cda1692365df71f6aedb4a410a3fd36a70c1a66)

--- calyx_sunfish.mk
+++ calyx_sunfish.mk
@@ -10,6 +10,12 @@ PRODUCT_NAME := calyx_sunfish
 PRODUCT_MODEL := Pixel 4a
 PRODUCT_BRAND := google
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sunfish \
+    PRIVATE_BUILD_DESC="sunfish-user 11 RQ3A.210905.001 7511028 release-keys" \
+
+BUILD_FINGERPRINT := google/sunfish/sunfish:11/RQ3A.210905.001/7511028:user/release-keys
+
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
