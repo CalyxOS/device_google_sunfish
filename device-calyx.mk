53756d0ac8ebf452202773a32c8fa27f2670cfe6
Michael Bestas <mkbestas@gmail.com>
2022-05-08 03:45:18 +0300

    Add privapp permissions for HbmSVManager
    Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

--- device-calyx.mk
+++ device-calyx.mk
@@ -16,6 +16,9 @@ PRODUCT_COPY_FILES += \
     device/google/sunfish/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
 # HBM
+PRODUCT_COPY_FILES += \
+    device/google/sunfish/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlay
 
